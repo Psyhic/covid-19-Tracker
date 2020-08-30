5946f8210dd09c844eca376302745399be920bea
Psyhic <sahilkumarrana18112000@gmail.com>
2020-08-30 15:35:39 +0530

    Updates

---
+++ precache-manifest.2bc94e22ceaa23d71892708f24fd42dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d0bbb46a78b7f0180bdf801c0c03c0",
+    "url": "/covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "8cf188d6ca749fc7020f",
+    "url": "/covid-19-Tracker/static/css/2.067ccc96.chunk.css"
+  },
+  {
+    "revision": "d1a087bec1bcb6d8e87d",
+    "url": "/covid-19-Tracker/static/css/main.d6140c17.chunk.css"
+  },
+  {
+    "revision": "8cf188d6ca749fc7020f",
+    "url": "/covid-19-Tracker/static/js/2.1954f588.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-19-Tracker/static/js/2.1954f588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a087bec1bcb6d8e87d",
+    "url": "/covid-19-Tracker/static/js/main.76e703ce.chunk.js"
+  },
+  {
+    "revision": "cc1acdd3574d99aee101",
+    "url": "/covid-19-Tracker/static/js/runtime-main.a593eb35.js"
+  }
+]);
